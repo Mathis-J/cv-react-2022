3d8027ef70a5306c47c6c84efee565350980de00
Mathis-J <mathisjancelii@gmail.com>
2022-10-12 20:55:21 +0200

    Base pour le CV react avec les routes

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Knowledges from './pages/Knowledges';
+import PortFolio from './pages/PortFolio';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/competences" element={<Knowledges />} />
+          <Route path="/portfolio" element={<PortFolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
