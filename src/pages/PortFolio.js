74fa51d036714360a1b55472682f28a82b1432e9
Mathis-J <mathisjancelii@gmail.com>
2022-10-20 19:27:27 +0200

    Toute la partie contact  + portfolio en cours

--- src/pages/PortFolio.js
+++ src/pages/PortFolio.js
@@ -4,10 +4,11 @@ import Navigation from '../components/Navigation';
 
 const PortFolio = () => {
     return (
-        <div>
+        <div className="portfolio">
             <Navigation />
-            <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="portfolioContent">
+                <h1> EN COURS</h1>
+            </div>
         </div>
     );
 };
