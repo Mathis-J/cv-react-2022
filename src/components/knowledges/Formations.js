0fa3aa309cdeda4ec7047dcef13f143d6e6c7422
Mathis-J <mathisjancelii@gmail.com>
2022-10-20 20:05:18 +0200

    Revert "Prettier"
    This reverts commit 6bfc18d39ddea1db4069675ebf4d5953cb3f7682.

--- src/components/knowledges/Formations.js
+++ src/components/knowledges/Formations.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 
 const Formations = () => {
-  return (
-    <div className="formation">
-      <h3>Formation</h3>
-      <div className="formation-1">
-        <h4>Concepteur développeur d'applications</h4>
-        <h5>ENI Ecole Informatique (2021 - 2022)</h5>
-      </div>
-      <div className="formation-2">
-        <h4>Technicien son</h4>
-        <h5>STAFF (2018)</h5>
-      </div>
-      <div className="formation-3">
-        <h4>BAFA</h4>
-        <h5>UFCV (2016)</h5>
-      </div>
-    </div>
-  );
+    return (
+        <div className="formation">
+            <h3>Formation</h3>
+            <div className="formation-1">               
+                <h4>Concepteur développeur d'applications</h4>
+                <h5>ENI Ecole Informatique (2021 - 2022)</h5>              
+            </div>
+            <div className="formation-2">               
+                <h4>Technicien son</h4>
+                <h5>STAFF (2018)</h5>                
+            </div>
+            <div className="formation-3">
+                <h4>BAFA</h4>
+                <h5>UFCV (2016)</h5>               
+            </div>
+        </div>
+    );
 };
 
 export default Formations;
--- src/components/knowledges/Languages.js
+++ src/components/knowledges/Languages.js
@@ -1,41 +1,41 @@
-import React, { Component } from "react";
-import ProgressBar from "./ProgressBar";
+import React, { Component } from 'react';
+import ProgressBar from './ProgressBar';
 
 class Languages extends Component {
-  state = {
+state = {
     languages: [
-      { id: 1, value: "JavaScript", xp: 2 },
-      { id: 2, value: "PHP", xp: 1.2 },
-      { id: 3, value: "TypeScript", xp: 1.2 },
-      { id: 4, value: "CSS", xp: 2 },
-      { id: 5, value: "HTML5:", xp: 2 },
+        {id: 1, value: "JavaScript", xp:2},
+        {id: 2, value: "PHP", xp:1.2},
+        {id: 3, value: "TypeScript", xp:1.2},
+        {id: 4, value: "CSS", xp:2},
+        {id: 5, value: "HTML5:", xp:2}
     ],
     frameworks: [
-      { id: 1, value: "React", xp: 1.5 },
-      { id: 2, value: "Sass", xp: 1.5 },
-      { id: 3, value: "Symfony", xp: 1 },
-    ],
-  };
+        {id: 1, value: "React", xp:1.5},
+        {id: 2, value: "Sass", xp:1.5},
+        {id: 3, value: "Symfony", xp:1}
+    ]
+}
 
-  render() {
-    let { languages, frameworks } = this.state;
+    render() { 
+        let {languages, frameworks} = this.state;
 
-    return (
-      <div className="languagesFrameworks">
-        <ProgressBar
-          title="Languages"
-          languages={languages}
-          className="languagesDisplay"
-        />
+        return (
+            <div className="languagesFrameworks">
+                <ProgressBar
+                    title="Languages"
+                    languages={languages}
+                    className="languagesDisplay"
+                />
 
-        <ProgressBar
-          title="Frameworks & librairies"
-          languages={frameworks}
-          className="frameworksDisplay"
-        />
-      </div>
-    );
-  }
+                <ProgressBar
+                    title="Frameworks & librairies"
+                    languages={frameworks}
+                    className="frameworksDisplay"
+                />
+            </div>
+        );
+    }
 }
 
 export default Languages;
